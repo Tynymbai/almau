f9a6c904e84942ae63a7f65635f5dca362005452
Akhmet Tynymbay <axmet.messi@gmail.com>
2023-03-04 17:07:15 +0600

    chnge

--- midterm/employees/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from employees.models import Employee
-
-
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'full_name', 'salary')
--- midterm/employees/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from employees.views import EmployeeViesSet
-
-router = DefaultRouter()
-router.register('employees', EmployeeViesSet)
-
-urlpatterns = []
-
-urlpatterns += router.urls
